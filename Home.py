c7ae3786a479767b80a346719c1f11dcff836f09
senspond20 <senspond@gmail.com>
2024-02-09 22:22:22 +0900

    wiki-search

---
+++ Home.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import re
+import time
+from modules.wiki import WikiSearch, OutputFormat
+
+
+def main():
+    st.set_page_config(page_title="Wikipedia Search", page_icon=None, layout="centered", initial_sidebar_state="auto",
+                       menu_items=None)
+    st.title("Wikipedia Search :sunglasses:")
+    option = st.selectbox("검색할 언어를 선택하세요", ("한국어(ko)", "영어(en)", "일본어(ja)", "중국어(zh)"))
+    language = re.sub(r"[ㄱ-힣()]", "", option)
+
+    keyword = st.text_input(label="검색할 키워드를 입력하시고 Enter 또는 검색 버튼을 클릭하세세요", value="")
+    button = st.button(type="primary", label="검색")
+
+    if keyword or button:
+        if keyword == "":
+            st.warning('키워드를 입력해주세요', icon="⚠️")
+        else:
+            st.write("")
+            wiki = WikiSearch(language, OutputFormat.HTML)
+            re_keyword, page = wiki.search(keyword)
+            with st.spinner(f"{option} 위키백과에서 '{re_keyword}' 검색 중입니다..."):
+                time.sleep(3)
+
+            if not page.exists():
+                st.error(f"{option} 위키백과에서 '{re_keyword}' 검색된 결과가 없어요...", icon="⚠️")
+            else:
+                st.write("link : " + page.fullurl)
+                st.write("<h4>위키 검색결과 요약</h4>", unsafe_allow_html=True)
+                st.write(page.summary, unsafe_allow_html=True)
+
+    st.image(
+        "https://images.unsplash.com/photo-1657256031790-e898b7b3f3eb?q=80&w=4032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+
+if __name__ == "__main__":
+    main()
---
+++ modules/wiki.py
@@ -0,0 +1,40 @@
+import wikipediaapi
+import re
+
+class OutputFormat():
+    WIKI = 1
+    HTML = 2
+
+
+class WikiSearch():
+    wiki = None
+
+    def __init__(self, language: str, format: OutputFormat):
+        """
+        :param language: "ko"/"en" ..
+        :param format: WIKI (텍스트 형식)/ HTML
+        """
+        params = {
+            "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+            "language": language,
+            "extract_format": format
+        }
+        self.wiki = wikipediaapi.Wikipedia(**params)
+
+    def search(self, keyword: str):
+        """
+        :param keyword: 검색할 키워드
+        :return: page
+        """
+        # 한글,영문,숫자 외 이상한 문자 제거
+        re_keyword = re.sub(r"[^ㄱ-힝a-zA-Z0-9]", "", keyword)
+
+        return re_keyword, self.wiki.page(re_keyword)
+
+
+if __name__ == "__main__":
+    wiki_ko = WikiSearch("ko", OutputFormat.WIKI)
+    re_keyword, page = wiki_ko.search("인공지능")
+    print(page.title)
+    # print(page.summary)
+    print(page.text)
